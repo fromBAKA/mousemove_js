f3b4e538ebb784275c812ca4b2d9aba5a7a84274
Nicolas To <107409156+fromBAKA@users.noreply.github.com>
2022-12-06 16:23:52 +0100

    Add files via upload

---
+++ Exercice mousemove JS/index.js
@@ -0,0 +1,16 @@
+// Créer 3 ronds de tailles différentes (dont un qui remplacera la souris)
+
+// Ajouter un événement sur la fenetre (window) puis animer la position de ces ronds (top, left injecter "e")
+
+// S'assurer que les liens sont clickables
+
+// Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+
+const mouses = document.querySelectorAll(".mouse");
+
+window.addEventListener("mousemove", (e) => {
+    mouses.forEach((mouse) => {
+        mouse.style.top = e.pageY + "px";
+        mouse.style.left = e.pageX + "px";
+    });
+});
